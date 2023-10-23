aec2273c03e512173c8a24f18145051cec857532
DonFernando20 <fernando.20.celis@gmail.com>
2023-10-23 17:22:30 +1100

    Number value fix

--- src/Components/ResultsTable/ResultsTable.js
+++ src/Components/ResultsTable/ResultsTable.js
@@ -1,8 +1,8 @@
 const formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
-  minimumFractionDigits: 2,
-  maximumFractionDigits: 2,
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
 });
 
 const ResultsTable = (props) => {
